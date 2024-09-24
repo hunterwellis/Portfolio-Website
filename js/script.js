b233bc80257c03306f89838b21c6ccdefe157b68
Hunter Ellis <elliswhunter@gmail.com>
2024-09-24 12:21:10 -0400

    init commit

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+if (document.getElementById('about-section-link')) {
+  document.getElementById('about-section-link').addEventListener('click', () => {
+    document.getElementById('about-section').scrollIntoView({behavior: "smooth"})
+  })
+}
